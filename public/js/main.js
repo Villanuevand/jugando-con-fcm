ed66cdf24083b52a6a0c18a16ff2d412f9eaa20d
Andres Villanueva <andresvillanuevab@gmail.com>
2018-10-31 20:16:45 -0300

    refactor(app): add some enhancements

--- public/js/main.js
+++ public/js/main.js
@@ -3,53 +3,60 @@ const messagingContainer = document.getElementById('messaging-container');
 const userName = document.getElementById('userName');
 const logOut = document.getElementById('logOut');
 
-document.addEventListener('DOMContentLoaded', function() {
-  initFirebaseUI();
+
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.disableAutoSignIn();
+
+function getUIConfig () {
+  return {
+    'callbacks': {
+      'signInSuccessWithAuthResult': function(authResult) {
+        logInSuccess(authResult);
+      },
+      'signInFailure': function(error) {
+        return handleUIError(error);
+      },
+      'uiShown': function() {
+        document.getElementById('loading').style.display = 'none';
+      }
+    },
+    'signInOptions': [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+  };
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  //document.getElementById('loading').style.display = 'none';
+  user ? logInSuccess(user) : logOutSuccess();
 });
 
 
-function initFirebaseUI () {
-  const uiConfig = {
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult) {
-        logInSuccess(authResult);
-      },
-      signInFailure: function(error) {
-        return handleUIError(error);
-      },
-      uiShown: function() {
-        document.getElementById('loading').style.display = 'none';
-      }
-    }
-
-  };
-  const ui = new firebaseui.auth.AuthUI(firebase.auth());
-  ui.start('#firebaseui-auth-container', uiConfig);
-}
-
-function logInSuccess (authResult) {
+function logInSuccess (user) {
+  console.log('Login auth result', user);
   firebaseUiContainer.style.display = 'none';
-  userName.innerHTML = `, ${ authResult.user.displayName}`;
   messagingContainer.style.display = 'block';
-  console.log('Login auth result', authResult);
-  return true;
+  userName.textContent = user.displayName;
 }
 
-function logOutSuccess (authResult) {
-  firebaseUiContainer.style.display = 'block';
+function logOutSuccess () {
   userName.innerHTML = ', desconocido';
+  firebaseUiContainer.style.display = 'block';
   messagingContainer.style.display = 'none';
-  console.log('Logout auth result', authResult);
-  return false;
+  userName.innerHTML = 'desconocido';
+  ui.start('#firebaseui-auth-container', getUIConfig());
 }
 
-logOut.addEventListener('click', function () {
-  firebase.auth().signOut().then(function() {
-    logOutSuccess();
-  }).catch(function(error) {
-    console.log('no cerro, algo paso', error)
+
+
+const initApp  = function () {
+  logOut.addEventListener('click', function () {
+    firebase.auth().signOut().then(function() {
+      logOutSuccess();
+    }).catch(function(error) {
+      console.log('no cerro, algo paso', error)
+    });
   });
-})
+};
+
+window.addEventListener('load', initApp);
