b99ebc3e8b631933c11bb1ba76cf6058713b80d0
Andres Villanueva <andresvillanuevab@gmail.com>
2018-11-13 19:37:06 -0300

    refactor(sw): add background notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,4 +2,15 @@ importScripts('/__/firebase/5.5.6/firebase-app.js');
 importScripts('/__/firebase/5.5.6/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
 
-firebase.messaging();
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('mensahe bacjground', payload);
+
+  var notificationTitle = 'Background mensaje de fondo';
+  var notificationOptions = {
+    body: 'Mensaje de prueba.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
