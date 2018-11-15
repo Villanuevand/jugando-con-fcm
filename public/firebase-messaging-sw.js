787b8c9b09f6953f4f5694c798ff5aa5ff452645
Andres Villanueva <andresvillanuevab@gmail.com>
2018-11-15 11:38:40 -0300

    refactor(sw): remove ununsed code

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,15 +2,4 @@ importScripts('/__/firebase/5.5.6/firebase-app.js');
 importScripts('/__/firebase/5.5.6/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
 
-const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('mensahe bacjground', payload);
-
-  var notificationTitle = 'Background mensaje de fondo';
-  var notificationOptions = {
-    body: 'Mensaje de prueba.',
-    icon: '/firebase-logo.png'
-  };
-
-  return self.registration.showNotification(notificationTitle, notificationOptions);
-});
+firebase.messaging();
