b70cabdb74569f480df143ffd50e7a714c587c69
Andres Villanueva <andresvillanuevab@gmail.com>
2018-11-17 07:28:12 -0400

    refactor(functions): add comments

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,19 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+// Send Notifications, se disparará cuando se escriba en /notifications/{notificationId}
 exports.sendNotifications = functions.database.ref('/notifications/{notificationId}').onWrite((change, context) => {
 
+  // Obteniendo los datos que están siendo escritos
   const rawData = change.after.val();
+  // ID de notificación
   const notificationID = context.params.notificationId;
+  // Verifica si existe datos antes del evento
   if (change.before.exists()) { return null;}
+  // Si no existen datos despues de la realizacion del evento
   if (!change.after.exists()) { return null;}
+
+  // Construcción de la notificacion
   const payload = {
     notification: {
       title: `Nuevo mensaje de: ${rawData.user}!`,
@@ -18,7 +25,7 @@ exports.sendNotifications = functions.database.ref('/notifications/{notification
   };
 
 
-  // Clean invalid tokens
+  // Invalidación de TOkens
   function cleanInvalidTokens(tokensWithKey, results) {
     const invalidTokens = [];
 
@@ -39,7 +46,7 @@ exports.sendNotifications = functions.database.ref('/notifications/{notification
     return Promise.all(invalidTokens);
   }
 
-
+// Obteniendo todos los tokens para hacer el envio de las notificaciones
   return admin.database().ref('/tokens').once('value').then((data) => {
     if ( !data.val() ) return;
 
@@ -54,7 +61,7 @@ exports.sendNotifications = functions.database.ref('/notifications/{notification
         key: key
       });
     }
-
+  // Enviando las notificaciones a todos los dispositivos
     return admin.messaging().sendToDevice(tokens, payload)
       .then((response) => cleanInvalidTokens(tokensWithKey, response.results))
       .then(() => admin.database().ref('/notifications').child(notificationID).remove())
